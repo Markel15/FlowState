f9abd9432bb0f70d30e2917a8dd2a3a949dd71d5
Markel15 <Enemar.h@gmail.com>
2026-09-12 03:08:04 +0200

    feat: delete ripple effect selecting a column
    Now the indication to show that a columns was selected is via the indication dot that appears above/inside the columns.

--- feature/habits/src/main/java/com/markel/flowstate/feature/habits/components/NumericWeekBar.kt
+++ feature/habits/src/main/java/com/markel/flowstate/feature/habits/components/NumericWeekBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -24,6 +25,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -160,7 +162,12 @@ fun NumericWeekBar(
     )
 
     Column(
-        modifier = modifier.clickable(enabled = isSelectable, onClick = onClick),
+        modifier = modifier.clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null,
+            enabled = isSelectable,
+            onClick = onClick
+        ),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom
     ) {
