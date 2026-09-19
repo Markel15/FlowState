30f52b3821cf3c4f0812b0a41895080ebe95517c
Markel15 <Enemar.h@gmail.com>
2026-09-19 20:17:24 +0200

    feat: bump version name
    incoming version: v3.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 31 // Android 12
         targetSdk = 36
         versionCode = 1
-        versionName = "3.5.1"
+        versionName = "3.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
